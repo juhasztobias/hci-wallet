148b3a7eeb43b85168646a19b837f7f31057a53e
Tobias Juhasz <tjuhasz@itba.edu.ar>
2024-10-24 18:13:51 -0300

    Enhance SidebarItem to track active state based on route; ensure active class is applied when matches current path

--- src/components/Sidebar/SidebarItem.vue
+++ src/components/Sidebar/SidebarItem.vue
@@ -1,5 +1,15 @@
 <script setup>
-    defineProps(['item'])
+    import { useRoute } from 'vue-router'
+    import { ref, watch } from 'vue'
+
+    const { item } = defineProps(['item'])
+    const isActive = ref(false);
+    const route = useRoute();
+
+    watch(() => route.path, () => {
+        isActive.value = route.path === item.to;
+    })
+
 </script>
 <template>
     <router-link :to="item.to">
@@ -7,6 +17,10 @@
             class="tw-bg-primary-500 tw-text-primary-100 hover:tw-bg-primary-400
             tw-font-medium tw-rounded-md tw-px-3 tw-py-2 tw-cursor-pointer tw-flex tw-items-center 
             tw-gap-2 tw-transition-all tw-duration-200 tw-ease-in-out"
+
+            :class="{
+                'tw-bg-primary-400': isActive,
+            }"
         >
             <!-- Example icon -->
             <v-icon>{{ item.icon }}</v-icon>
