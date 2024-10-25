52ccf594c9fa555e3bcd2f8943c7e5fc3c8d4356
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2024-10-24 23:14:13 -0300

    Recovery Page

--- src/pages/auth/recovery.vue
+++ src/pages/auth/recovery.vue
@@ -1,11 +1,54 @@
 <script>
-
+    export default {
+    data() {
+        return {
+        recoveryMethods: [
+            { title: 'Mensaje de texto', icon: 'mdi-message' },
+            { title: 'Mensaje de Whatsapp', icon: 'mdi-whatsapp' },
+            { title: 'Correo electrónico', icon: 'mdi-email' }
+        ]
+        };
+    }
+    };
+    
 </script>
 
+
 <template>
-
-</template>
-
+    <v-container class="flex justify-center items-center min-h-screen bg-blue-900">
+        <!-- Header -->
+        <div class="tw-text-2xl tw-text-primary-600 tw-font-semibold tw-text-left">
+          Restablecer contraseña
+        </div>
+        <div class="tw-text-xl tw-text-primary-600 tw-font-light tw-text-left">
+          Selecciona un método de recuperación
+        </div>
+  
+        <!-- Recovery Options -->
+        <v-list class="mt-6">
+          <v-list-item v-for="(method, index) in recoveryMethods" :key="index">
+            <div
+              class="tw-flex tw-items-center tw-justify-between tw-w-full hover:tw-bg-slate-200 active:tw-bg-slate-400 tw-cursor-pointer tw-px-6 tw-py-4 tw-rounded-lg"
+              @click="handleRecoveryMethod(method.title)"
+            >
+              <div class="tw-flex tw-items-center">
+                <v-icon left class="tw-mr-3">{{ method.icon }}</v-icon>
+                {{ method.title }}
+              </div>
+              <v-icon>mdi-chevron-right</v-icon>
+            </div>
+          </v-list-item>
+        </v-list>
+  
+        <!-- Cancel Button -->
+        <div class="tw-items-left mt-6">
+          <v-btn variant="text" color="primary">
+            <v-icon>mdi-arrow-left</v-icon>
+            Cancelar
+          </v-btn>
+        </div>
+    </v-container>
+  </template>
 <route lang="yaml">
 meta:
     layout: auth.layout
