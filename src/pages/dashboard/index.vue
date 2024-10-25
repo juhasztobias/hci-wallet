afcbc05f02ff3ce0db29a553d42e29f5398afb99
Tobias Juhasz <tjuhasz@itba.edu.ar>
2024-10-24 22:25:27 -0300

    Implement BillGraph component for visualizing expenses; update dashboard and playground with new chart integration

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,7 +1,23 @@
 <template>
-    <h1>Home</h1>
-    <p>This is the home page</p>
+    <main class="tw-grid tw-grid-cols-[1fr,.4fr] tw-gap-3 tw-p-2 tw-items-start">
+        <section class="tw-flex tw-flex-col tw-gap-3 tw-items-center tw-justify-center">
+            <v-card class="tw-w-full tw-min-h-[600px] tw-p-4"></v-card>
+            <v-card class="tw-w-full tw-min-h-[200px] tw-p-4"></v-card>
+        </section>
+        <aside class="tw-flex tw-gap-3 tw-flex-col">
+            <BillGraph :series_data="series_data"/>
+            <BillGraph :series_data="series_data"/>
+        </aside>
+    </main>
 </template>
+
+<script setup>
+    const series_data = [
+        { name: 'Comida', value: 1000, color: '#FB67CA' }, 
+        { name: 'Entretenimiento', value: 2000, color: '#FFA84A' },
+        { name: 'Transporte', value: 1000, color: '#04BFDA' }, 
+    ]
+</script>
 
 <route lang="yaml">
 meta:
--- src/pages/playground/index.vue
+++ src/pages/playground/index.vue
@@ -1,73 +1,18 @@
 <template>
     <h1>Playground</h1>
-    <div
-        class="tw-flex tw-items-center"
-    >
-        <apexchart 
-            type="radialBar"
-            :options="chartOptions"
-            :series="scaledSeries"
-        /> 
+    <div class="tw-flex tw-items-center tw-justify-center tw-h-screen">
+        <BillGraph 
+            :series_data="series_data"
+        />
     </div>
+
 </template>
 
-<script setup> 
+<script setup>
 
-    const series = [ 500, 20, 30 ];
-    const total = series.reduce((a, b) => a + b, 0);
-    const scaledSeries = series.map(s => s / total * 100);
-
-    const formatCurrency = (value) => {
-        const idx = scaledSeries.indexOf(value);
-        console.log(idx);
-        return new Intl.NumberFormat('es-AR', {
-            style: 'currency',
-            currency: 'ARS',
-        }).format(series[idx]);
-    }
-
-    const chartOptions = {
-        chart: {
-            height: 350,
-            type: 'radialBar',
-        },
-        plotOptions: {
-            radialBar: {
-                dataLabels: {
-                    value: {
-                        offsetY: 0,
-                        fontWeight: 'bold',
-                        formatter: formatCurrency,
-                    },
-                    total: {
-                        show: true,
-                        label: 'Gasto total',
-                        formatter: function (w) {
-                            return new Intl.NumberFormat('es-AR', {
-                                style: 'currency',
-                                currency: 'ARS',
-                            }).format(total);
-                        },
-                    },
-                },
-                hollow: {
-                    margin: 5,
-                    size: '40%',
-                    background: 'transparent',
-                    image: undefined,
-                    position: 'front',
-                },
-                track: {
-                    margin: 10, // margin is in pixels
-                },
-
-            },   
-        },
-        stroke: {
-            lineCap: 'round',
-        },
-        labels: ['Vuetify', 'Vue', 'Apex Charts'],
-    }
-
-    
+    const series_data = [
+        { name: 'Comida', value: 1000, color: '#FB67CA' }, 
+        { name: 'Entretenimiento', value: 2000, color: '#FFA84A' },
+        { name: 'Transporte', value: 1000, color: '#04BFDA' }, 
+    ]
 </script>
