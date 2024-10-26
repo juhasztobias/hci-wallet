ba449a02f32b72c45de456281b358cfd0d4a3584
RamiroGarciaAr <67532049+RamiroGarciaAr@users.noreply.github.com>
2024-10-26 00:20:14 -0300

    TransferSuccess

--- src/pages/test/card.vue
+++ src/pages/test/card.vue
@@ -1,11 +1,17 @@
 <script>
 import TransferDetails from '@/components/Transaction/TransferDetails.vue';
 import TransferError from '@/components/Transaction/TransferError.vue';
+import TransferSuccess from '@/components/Transaction/TransferSuccess.vue';
 
 
 </script>
 <template>
     <v-card>
-        <TransferError/>
+        <TransferSuccess
+            amount="123.43"
+            receiverName="Test Name"
+            bankName="Galicia"
+            voucherNumber="123456"
+        />
     </v-card>
 </template>
