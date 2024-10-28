c141e963681d61cf8475bd2290f873cd407c7687
Tobias Juhasz <tjuhasz@itba.edu.ar>
2024-10-27 21:35:35 -0300

    Refactor MoneyInput component for improved input handling and styling

--- src/components/Input/MoneyInput.vue
+++ src/components/Input/MoneyInput.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="flex flex-col items-center">
+    <div class="tw-flex tw-flex-col tw-items-center tw-text-primary-500">
         <!-- Etiqueta -->
         <label class="text-center text-xl font-semibold mb-4 text-gray-800">Ingrese el monto</label>
 
         <!-- Campo de entrada -->
-        <div class="tw-max-w-72 tw-flex tw-gap-1">
+        <div class="tw-max-w-fit tw-flex tw-gap-1">
             <!-- Signo de dólar -->
-            <span class="tw-text-2xl">$</span>
+            <span class="tw-text-2xl tw-font-bold tw-self-center">$</span>
             <!-- Input para la parte entera -->
-            <span class="tw-outline-none tw-flex-1 tw-px-2 tw-text-4xl tw-text-gray-800" contenteditable="true"
-                @input="onIntegerInput" @keydown="onIntegerKeydown" ref="integerInput" data-placeholder="0"></span>
-
-            <!-- Punto decimal -->
-            <span class="tw-text-2xl tw-text-gray-800">.</span>
-
+            <span
+                class="tw-outline-none tw-font-bold tw-flex-1 tw-px-2 tw-text-4xl tw-text-primary-500 tw-inline-block integer-part"
+                contenteditable="true" @input="onIntegerInput" @keydown="onIntegerKeydown" ref="integerInput"
+                data-placeholder="0"></span>
             <!-- Input para la parte decimal -->
-            <span class="tw-outline-none tw-px-2 tw-text-2xl tw-text-gray-800" contenteditable="true"
-                @input="onDecimalInput" @keydown="onDecimalKeydown" ref="decimalInput" data-placeholder="00"></span>
+            <span class="tw-outline-none tw-font-bold tw-px-2 tw-text-xl tw-text-primary-500 tw-inline-block"
+                contenteditable="true" @input="onDecimalInput" @keydown="onDecimalKeydown" ref="decimalInput"
+                data-placeholder="00"></span>
         </div>
 
         <!-- Texto secundario -->
-        <p class="mt-4 text-gray-600 text-base">
+        <p class="mt-4 text-gray-600 text-base tw-text-primary-300">
             Dinero disponible: <span class="font-semibold">$100</span>
         </p>
     </div>
@@ -52,36 +51,39 @@ export default {
             // Si la parte decimal está vacía, regresar el foco a la parte entera
             if (this.decimalPart.length === 0) {
                 this.$refs.integerInput.focus();
-                this.setCursorToEnd(this.$refs.integerInput); // Asegurar que el cursor esté al final
+                this.setCursorToEnd(this.$refs.integerInput);
             }
         },
         onIntegerKeydown(event) {
             if (event.key === '.' || event.key === ',') {
-                event.preventDefault(); // Evitar que el carácter se agregue al input
+                event.preventDefault();
                 this.$refs.decimalInput.focus();
-                this.setCursorToEnd(this.$refs.decimalInput); // Asegurar que el cursor esté al final
+                this.setCursorToEnd(this.$refs.decimalInput);
             }
         },
         onDecimalKeydown(event) {
             if (event.key === 'Backspace' && this.decimalPart.length === 0) {
-                // Si la parte decimal está vacía y se presiona Backspace, regresar el foco a la parte entera
                 event.preventDefault();
                 this.$refs.integerInput.focus();
-                this.setCursorToEnd(this.$refs.integerInput); // Asegurar que el cursor esté al final
+                this.setCursorToEnd(this.$refs.integerInput);
             }
         },
         setCursorToEnd(el) {
-            // Mover el cursor al final del contenido
             const range = document.createRange();
             const sel = window.getSelection();
             range.selectNodeContents(el);
             range.collapse(false);
             sel.removeAllRanges();
             sel.addRange(range);
-            // Asegurar que el elemento esté enfocado
             el.focus();
         },
     },
+    computed: {
+        totalPart() {
+            // Parse to number
+            return parseFloat(this.integerPart + "." + this.decimalPart);
+        }
+    }
 };
 </script>
 
@@ -97,6 +99,11 @@ export default {
 }
 
 [contenteditable] {
-    min-width: 0.5em;
+    display: block;
+    /* Convertir a bloque en línea */
+    max-width: fit-content;
+    /* Establecer un ancho mínimo */
+    min-height: 2ch;
+    /* Establecer una altura mínima */
 }
 </style>
